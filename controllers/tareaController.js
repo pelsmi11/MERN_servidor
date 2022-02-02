13df8f1605586772d99218e1c961d1cd0fdb9e11
pelsmi11 <hmartinezmoreira81@gmail.com>
2022-02-02 13:18:57 -0600

    heroku 4

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -1,7 +1,7 @@
 const Tarea = require('../models/Tarea');
 const Proyecto = require('../models/Proyecto');
 const {validationResult}= require('express-validator');
-const { restart } = require('nodemon');
+// const { restart } = require('nodemon');
 
 
 // Crea una nueva tarea
